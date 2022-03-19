7d9e553a4ca79b6b3e6a2595a5a65da48c4d4bf4
Alec Harvey <ruinhard@gmail.com>
2022-03-18 21:59:04 -0500

    attempted to fixed query.sql

--- db/query.sql
+++ db/query.sql
@@ -1,5 +1,11 @@
-select
-employee.first_name as first_name, employee.last_name as last_name, role.id as role_id,employee.manager_id as manager_id from employee
+/*select
+employee.first_name as first_name, employee.last_name as last_name, role.id as role_id,employee.manager_id as manager_id
+ from employee
 
-/*join employee on employee.manager_id=manager_id;*/
-join role on employee.role_id=role.id;
+join role on employee.role_id=role.id;*/
+select t1.first_name, t1.last_name, role.title,role.salary,department.name as Department,t2.manager_id
+from employee t1, employee t2
+
+join employee t2 on t1.manager_id=t2.manager_id
+join role on t1.role_id=role.id
+join department on role.department_id=department.id;
